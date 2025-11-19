8f8cc207cb82a3ee3953f18dfde13e8cde9492b1
jcastanedachavarro-lgtm <jcastanedachavarro@gmail.com>
2025-11-19 12:17:32 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+let who = ['My dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let excuse = [who[Math.floor(Math.random() * who.length)] + " " +
+             action[Math.floor(Math.random() * action.length)] + " " +
+             what[Math.floor(Math.random() * what.length)] + " " +
+             when[Math.floor(Math.random()* when.length)]
+            ];
+
+console.log(excuse); 
+
+    
+
+const btn = document.querySelector('button')
+  btn.addEventListener('click', () => {
+    let texto = document.querySelector('#excusa') 
+    texto.innerHTML = excuse
+  })
+
 };
+
